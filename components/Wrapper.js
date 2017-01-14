f0c5fd7b1866a52f085983d5f45dff6af8392578
Joel Burget <joelburget@gmail.com>
2017-01-14 14:18:44 -0500

    Update wrapper.

--- components/Wrapper.js
+++ components/Wrapper.js
@@ -5,8 +5,6 @@ import Link from 'next/link';
 import remark from 'remark';
 import reactRenderer from 'remark-react';
 
-import containerStyle from '../components/containerStyle';
-
 const remarkOpts = {
   remarkReactComponents: {
     a: Link,
@@ -19,7 +17,7 @@ export default class Wrapper extends React.Component {
   }
 
   render() {
-    const { chrome, children: content } = this.props;
+    const { children: content } = this.props;
 
     return (
       <div>
@@ -36,14 +34,74 @@ export default class Wrapper extends React.Component {
               <Link href="/">/home</Link>
             </nav>
             {remark().use(reactRenderer, remarkOpts).process(content).contents}
-            <style jsx>{`
-              nav {
-                margin-bottom: 80px;
-                font-size: 14px;
-              }
-            `}</style>
           </div>
-          {containerStyle}
+          <style jsx global>{`
+        .host {
+          margin: 0 auto;
+          padding: 40px 20px;
+          max-width: 900px;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera San;
+          text-rendering: geometricPrecision;
+        }
+        nav {
+          margin-bottom: 80px;
+          font-size: 14px;
+        }
+
+        nav a {
+          display: inline-block;
+          margin-right: 20px;
+        }
+
+        h1, h2, h3 {
+          font-weight: 700;
+          color: #000;
+        }
+        h1 {
+          font-size: 16px;
+        }
+        h2 {
+          font-size: 14px;
+        }
+        h3 {
+          font-size: 13px;
+        }
+        h1::before {
+          content: '# ';
+        }
+        h2::before {
+          content: '## ';
+        }
+        h3::before {
+          content: '### ';
+        }
+        p, li {
+          color: #424242;
+          font-size: 13px;
+          line-height: 22px;
+        }
+
+        em::before, em::after {
+          content: '/';
+        }
+
+        strong::before, strong::after {
+          content: '*';
+        }
+
+        li > p {
+        }
+
+        /* not sure if a good idea / used? */
+        u::before, u::after {
+          content: '_';
+        }
+        /*
+        li {
+          fontSize: 13px;
+        }
+        */
+          `}</style>
         </div>
       </div>
     );
