c97014310f17164c1fb95f7a7cfa82eb3928b4fb
Joel Burget <joelburget@gmail.com>
2017-05-16 09:08:03 -0400

    Add draft of /next-blog.

---
+++ pages/next-blog.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+import Wrapper from '../components/Wrapper';
+import content from './next-blog.md';
+
+export default () => <Wrapper chrome={false}>{content}</Wrapper>;
